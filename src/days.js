d1410b4012dd748cd6753bd04f404b207ab67af3
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2024-07-03 20:15:04 -0300

    Implement days and weather as well as exceptions on eslint eslint will now ignore the webpack configuration files, to prevent it from erroneously flagging the commonjs exports present on those as errors. implement days module to gather the data for the days, instead of using weather module for all the days refactor weather to make use of Promise.all to run promises concurrently instead of having to await each fetch

---
+++ src/days.js
@@ -0,0 +1,44 @@
+export default class Days {
+  static icons = {
+    // "temperature",
+    // "humidity",
+    // "condition",
+    // "moon",
+    // "wind",
+    // "tide",
+  };
+  constructor({ astro, date, day }, location) {
+    this.location = location;
+    this.temperature = {
+      average: { celsius: day.avgtemp_c, fahrenheit: day.avgtemp_f },
+      min: { celsius: day.mintemp_c, fahrenheit: day.mintemp_f },
+      max: { celsius: day.maxtemp_c, fahrenheit: day.maxtemp_f },
+    };
+    this.humidity = day.avghumidity;
+    this.condition = day.condition.text;
+    this.moon = astro.moon_phase;
+    //use the date library that I used before
+    this.date = date;
+    this.wind = { kph: day.maxwind_kph, mph: day.maxwind_mph };
+    // this.tide = tide;
+    console.log(`inside days ${astro} ${day}`);
+    console.log(astro);
+    console.log(day);
+    console.log(this.toString());
+  }
+  static async load() {}
+  toString() {
+    return {
+      location: this.location,
+      temperature: this.temperature,
+      humidity: this.humidity,
+      condition: this.condition,
+      moon: this.moon,
+      date: this.date,
+      wind: this.wind,
+    };
+  }
+  render() {
+    //implement the render function
+  }
+}
---
+++ src/weather.js
@@ -0,0 +1,69 @@
+import Days from "./days.js";
+export default class Weather {
+  static weatherReport = {
+    location: null,
+    humidity: null,
+    temperature: null,
+    condition: null,
+    moon: null,
+    wind: null,
+    forecast: null,
+  };
+  //or should I use a constructor ? maybe the static initializator?
+  static init() {}
+  static async load(location) {
+    const k = "4e4dde871a60444190235252243006";
+    const url = "https://api.weatherapi.com/v1";
+    const searchString = location;
+    const days = 3;
+    const urls = {
+      current: `${url}/current.json?key=${k}&q=${searchString}`,
+      astronomy: `${url}/astronomy.json?key=${k}&q=${searchString}`,
+      forecast: `${url}/forecast.json?key=${k}&q=${searchString}&days=${days}`,
+    };
+
+    try {
+      const [currentStream, astronomyStream, forecastStream] =
+        await Promise.all([
+          fetch(urls.current),
+          fetch(urls.astronomy),
+          fetch(urls.forecast),
+        ]);
+
+      const [currentData, astronomyData, forecastData] = await Promise.all([
+        currentStream.json(),
+        astronomyStream.json(),
+        forecastStream.json(),
+      ]);
+
+      return { currentData, astronomyData, forecastData };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //peharps this could be split on a class that handles the astronomy fetch and processing?
+  static processData(currentData, astronomyData, forecastData) {
+    console.log(currentData);
+    const weatherReport = {
+      location: currentData.location.name,
+      temperature: currentData.current.temp_c,
+      humidity: currentData.current.humidity,
+      condition: currentData.current.condition.text,
+      moon: astronomyData.astronomy.astro.moon_phase,
+
+      wind: currentData.current.wind_kph,
+      forecast: forecastData.forecast.forecastday,
+    };
+    this.weatherReport = weatherReport;
+    console.log(weatherReport);
+    return weatherReport;
+  }
+  static processDays(forecast, location) {
+    console.log(forecast);
+    forecast.forEach((day) => {
+      console.log(day);
+      new Days(day, location);
+    });
+  }
+}
